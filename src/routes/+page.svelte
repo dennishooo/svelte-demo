ece0b882d29ec4aa08e282084953212c8e7cc974
dennishoooo <dennis@tecky.io>
2023-02-15 21:59:22 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let message = 'hello world';
+	export function greet() {
+		alert('welcome to svelte');
+	}
+</script>
+
+<h1>{message}</h1>
+<button on:click={greet}>greet</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
